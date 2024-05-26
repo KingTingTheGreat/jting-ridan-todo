cee477bce674179c47645da03839ae66aa08096c
Jeffrey Ting <jting@bu.edu>
2024-05-26 10:26:21 -0500

    formatting

--- components/entryCard.tsx
+++ components/entryCard.tsx
@@ -12,19 +12,21 @@ const EntryCard = ({ entry }: { entry: Entry }) => {
 			<p>Created at: {entry.createdAt}</p>
 			<p>Updated at: {entry.updatedAt}</p>
 			<p>Completed: {completed ? "Yes" : "No"}</p>
-			<svg 
-				onClick={()=>{setCompleted(!completed)}}
-				xmlns="http://www.w3.org/2000/svg" 
-				width="40" 
-				height="40" 
-				viewBox="0 0 24 24" 
-				fill={completed ? "lightgreen" : "none"} 
-				stroke="#000000" 
-				strokeWidth="2" 
-				strokeLinecap="round" 
-				strokeLinejoin="round"
-			>
-				<path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline>
+			<svg
+				onClick={() => {
+					setCompleted(!completed);
+				}}
+				xmlns="http://www.w3.org/2000/svg"
+				width="40"
+				height="40"
+				viewBox="0 0 24 24"
+				fill={completed ? "lightgreen" : "none"}
+				stroke="#000000"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round">
+				<path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+				<polyline points="22 4 12 14.01 9 11.01"></polyline>
 			</svg>
 		</div>
 	);
