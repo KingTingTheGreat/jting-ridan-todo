31d017763082ecc0e9fc442f03b2f57c49120ceb
Jeffrey Ting <jting@bu.edu>
2024-06-01 11:05:48 -0500

    new entry toggle

--- components/entriesView.tsx
+++ components/entriesView.tsx
@@ -14,6 +14,7 @@ const enum SortBy {
 const EntriesView = ({ password, attemptAuth }: { password: string; attemptAuth: boolean }) => {
 	const [entries, setEntries] = useState<Entry[]>([]);
 	const [sortBy, setSortBy] = useState<SortBy>(SortBy.LastUpdated);
+	const [createNewEntry, setCreateNewEntry] = useState<boolean>(false);
 
 	const authorize = async () => {
 		const myHeaders = new Headers();
@@ -38,18 +39,30 @@ const EntriesView = ({ password, attemptAuth }: { password: string; attemptAuth:
 	}, [password, attemptAuth]);
 
 	useEffect(() => {
+		console.log(sortBy);
 		switch (sortBy) {
 			case SortBy.DateCreated:
 				setEntries(
-					[...entries].sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
+					[...entries].sort((a, b) => {
+						const dateA = new Date(a.createdAt).getTime();
+						const dateB = new Date(b.createdAt).getTime();
+						return dateA - dateB;
+					})
 				);
+				break;
 			case SortBy.LastUpdated:
 				setEntries(
-					[...entries].sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime())
+					[...entries].sort((a, b) => {
+						const dateA = new Date(a.updatedAt).getTime();
+						const dateB = new Date(b.updatedAt).getTime();
+						return dateB - dateA;
+					})
 				);
+				break;
 
 			case SortBy.Title:
 				setEntries([...entries].sort((a, b) => a.title.localeCompare(b.title)));
+				break;
 
 			case SortBy.Complete:
 				setEntries(
@@ -57,21 +70,9 @@ const EntriesView = ({ password, attemptAuth }: { password: string; attemptAuth:
 						return b === a ? 0 : b ? -1 : 1;
 					})
 				);
+				break;
 		}
-	}, [sortBy]);
-
-	if (entries.length === 0 && attemptAuth) {
-		return (
-			<div>
-				<NewEntryCard
-					password={password}
-					appendEntry={(entry: Entry) => {
-						setEntries([...entries, entry]);
-					}}
-				/>
-			</div>
-		);
-	}
+	}, [sortBy, entries.length]);
 
 	if (!attemptAuth) {
 		return <></>;
@@ -94,18 +95,28 @@ const EntriesView = ({ password, attemptAuth }: { password: string; attemptAuth:
 
 	return (
 		<div>
-			<NewEntryCard
-				password={password}
-				appendEntry={(entry: Entry) => {
-					setEntries([...entries, entry]);
-				}}
-			/>
-			<div className="flex justify-end p-2">
+			<div className="flex justify-center p-2">
+				<button onClick={() => setCreateNewEntry(!createNewEntry)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="44"
+						height="44"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="#000000"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round">
+						<line x1="12" y1="5" x2="12" y2="19"></line>
+						<line x1="5" y1="12" x2="19" y2="12"></line>
+					</svg>
+				</button>
 				<select
 					className="w-max-content p-2 bg-inherit border border-black rounded-md"
 					onChange={(e) => {
 						setSortBy(e.target.value as SortBy);
-					}}>
+					}}
+					defaultValue={SortBy.LastUpdated}>
 					<option value={SortBy.DateCreated}>{SortBy.DateCreated}</option>
 					<option value={SortBy.LastUpdated}>{SortBy.LastUpdated}</option>
 					<option value={SortBy.Title}>{SortBy.Title}</option>
@@ -117,8 +128,8 @@ const EntriesView = ({ password, attemptAuth }: { password: string; attemptAuth:
 						setEntries([...entries].reverse());
 					}}>
 					<svg viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" fill="#000000" transform="rotate(90)">
-						<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-						<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+						<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+						<g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
 						<g id="SVGRepo_iconCarrier">
 							{" "}
 							<path d="M17.6,4.2l-4-3a1,1,0,0,0-1.05-.09A.977.977,0,0,0,12,2V4H1A1,1,0,0,0,1,6H12V8a.988.988,0,0,0,.55.89A.99.99,0,0,0,13.6,8.8l4-3a1,1,0,0,0,0-1.6Z"></path>{" "}
@@ -127,6 +138,17 @@ const EntriesView = ({ password, attemptAuth }: { password: string; attemptAuth:
 					</svg>
 				</div>
 			</div>
+
+			{createNewEntry && (
+				<NewEntryCard
+					password={password}
+					appendEntry={(entry: Entry) => {
+						console.log(entry);
+						setCreateNewEntry(false);
+						setEntries([...entries, entry]);
+					}}
+				/>
+			)}
 
 			{entries.map((entry) => (
 				<EntryCard key={entry.id} removeEntry={removeEntry} entryInitial={entry} password={password} />
